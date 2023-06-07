d671f20ec29bedaa6cc0e18d2eda8e968b06d052
김덕용 <yh100112@naver.com>
2023-06-07 10:12:53 +0900

    map_filter

--- 배열/map_filter_동시사용.js
+++ 배열/map_filter_동시사용.js
@@ -1,10 +1,11 @@
 let numbers = [273, 52, 103, 32, 57]
 
-let versions = numbers.map(item => { return String(item); }).filter(item => !isNaN(item));
+let versions = numbers.map(String).filter(item => !isNaN(item));
 // 숫자를 문자열로 바꾸고, numbers의 값이 NaN이 아닌 것만 versions에 새로운 배열의 값으로 넣음
 
 console.log(versions);
 
 /*
 실행결과
+[ '273', '52', '103', '32', '57' ]
 */
