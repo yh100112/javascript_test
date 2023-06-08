2ee3899abff828ec84a5a011e4edaa536e61ea52
김덕용 <yh100112@naver.com>
2023-06-08 17:21:19 +0900

    split

--- test.cpp
+++ test.cpp
@@ -8,5 +8,4 @@ int main(){
   cout.tie(NULL);
 
   cin >> a;
-  
 }
---
+++ 배열/split().js
@@ -0,0 +1,13 @@
+const str = "apple banana orange";
+
+const arr = str.split(" ");
+
+console.log(arr.length); // 3
+console.log(arr[0]); // apple
+console.log(arr[1]); // banana
+console.log(arr[2]); // orange
+
+
+const a = "apple";
+const aaaa = a.split(",");
+console.log(aaaa); // ['apple']
